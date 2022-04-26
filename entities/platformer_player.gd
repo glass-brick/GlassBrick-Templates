c9570e43eba5330a8c7b2dad635cf76423390a71
Julián Absatz <julianabsatz@gmail.com>
2022-04-26 14:53:15 +0200

    acme time, controller support, readable exported variables

--- entities/platformer_player.gd
+++ entities/platformer_player.gd
@@ -4,20 +4,24 @@ var velocity = Vector2()
 
 export (int) var health = 100
 
-var max_speed = 300
-var acceleration = 100
+export (int) var max_speed = 200
+export (int) var acceleration_frames = 5
+export (int) var decceleration_frames = 2
+onready var acceleration = float(max_speed) / float(acceleration_frames)
+onready var decceleration = float(max_speed) / float(decceleration_frames)
 var flipped = false
 
-export (int) var gravity = 1200
-var jump_speed = -600
-var jump_regulation_frames = 10
+export (float, 0, 5, 0.1) var air_time = 1
+export (float, 0, 20, 0.5) var max_jump_height = 3.0
+onready var player_height = $CollisionShape2D.get_shape().get_extents().y
+onready var jump_speed = -player_height * max_jump_height * 9 / air_time
+onready var gravity = -jump_speed * 2 / air_time
+var can_floor_jump = false
 var can_double_jump = true
-var jump_time = 0
-var jumping = false
+var jump_cancel_time = 0
 
-export (float) var dash_stop = 0.1
-export (float) var dash_anim_duration = 0.2
-var dash_speed = 1000
+export (float) var dash_speed = 1000
+export (float, 0, 0.5) var dash_duration = 0.2
 var dash_timer = 0
 var dash_direction = 1
 var dash_jumped = false
@@ -25,6 +29,8 @@ var dash_jumped = false
 export (float) var invincibility_time = 0.5
 var invincibility_counter = 0.0
 var invincibility = false
+
+export (float) var acme_time = 0.1
 
 onready var animated_sprite : AnimatedSprite = $AnimatedSprite
 onready var dash_trail : CPUParticles2D = $Trail
@@ -36,14 +42,10 @@ onready var smp = StateMachine.new(
 	[
 		'Floored',
 		'Airborne',
-		'Jump',
-		'Fall',
-		'Crouch',
 		'Dash',
 		'Dead',
 	]
 )
-
 
 func set_health(new_health):
 	health = max(new_health, 0)
@@ -52,33 +54,22 @@ func set_health(new_health):
 
 
 func process_horizontal_move():
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var dash = Input.is_action_just_pressed('dash')
+	var direction := Input.get_axis('ui_left', 'ui_right')
+	var dash_just_pressed := Input.is_action_just_pressed('dash')
 
-	if (left or right) and not (left and right):
-		if right and velocity.x < max_speed:
-			velocity.x += acceleration
-			if dash_direction == -1:
-				dash_timer += dash_stop
-		elif left and velocity.x > -max_speed:
-			velocity.x -= acceleration
-			if dash_direction == 1:
-				dash_timer += dash_stop
+	if not is_zero_approx(direction):
+		velocity.x = clamp(velocity.x + acceleration * direction, -max_speed * abs(direction), max_speed * abs(direction))
+		if direction > 0:
+			flip_right()
+		else:
+			flip_left()
 	else:
 		if velocity.x > 0:
-			velocity.x = max(velocity.x - acceleration, 0)
+			velocity.x = max(velocity.x - decceleration, 0)
 		elif velocity.x < 0:
-			velocity.x = min(velocity.x + acceleration, 0)
-	
-	if left:
-		flip_left()
-	elif right:
-		flip_right()
+			velocity.x = min(velocity.x + decceleration, 0)
 
-	velocity.x = clamp(velocity.x, -max_speed, max_speed)
-
-	if dash and not (left and right) and (is_on_floor() or not dash_jumped):
+	if dash_just_pressed and (is_on_floor() or not dash_jumped):
 		smp.travel_to('Dash')
 
 func flip_left():
@@ -91,28 +82,31 @@ func flip_right():
 		flipped = false
 		scale.x = -1
 
-func process_jump_input():
-	var jump = Input.is_action_pressed('jump')
+func process_jump_input(delta: float):
+	var jump_pressed = Input.is_action_pressed('jump')
 	var jump_just_pressed = Input.is_action_just_pressed('jump')
 
-	if jump_just_pressed and (is_on_floor() or can_double_jump):
-		jumping = true
-		jump_time = jump_regulation_frames
+	if is_on_floor():
+		can_floor_jump = true
+	elif smp.time_since("Floored") > acme_time:
+		can_floor_jump = false
+
+	if jump_just_pressed and (can_floor_jump or can_double_jump):
 		velocity.y = jump_speed
 		dash_jumped = false
-		if not is_on_floor():
+		if can_floor_jump:
+			can_floor_jump = false
+		elif can_double_jump:
 			can_double_jump = false
 		animated_sprite.play('Jump' if can_double_jump else 'DoubleJump')
+		jump_cancel_time = air_time / 2.0
 
-	if jump_time > 0:
-		if not jump:
-			velocity.y -= jump_speed * jump_time / jump_regulation_frames
-			jump_time = 0
-			jumping = false
+	if jump_cancel_time > 0:
+		if not jump_pressed:
+			velocity.y = 0
+			jump_cancel_time = 0
 		else:
-			jump_time -= 1
-			if jump_time == 0:
-				jumping = false
+			jump_cancel_time -= delta
 
 
 func transited_state(_from, to):
@@ -120,8 +114,6 @@ func transited_state(_from, to):
 		"Floored":
 			can_double_jump = true
 			dash_jumped = false
-		"Crouch":
-			animated_sprite.play('Crouch')
 		"Dead":
 			health = 0
 			velocity = Vector2()
@@ -133,11 +125,10 @@ func transited_state(_from, to):
 			dash_direction = -1 if flipped else 1
 			if not is_on_floor():
 				dash_jumped = true
-			dash_timer = 0
 
 
-func process_state(state, delta):
-	match state:
+func _process(delta):
+	match smp.state:
 		"Airborne":
 			if not can_double_jump:
 				animated_sprite.play('DoubleJump')
@@ -147,37 +138,30 @@ func process_state(state, delta):
 				animated_sprite.play('Fall')
 			if is_on_floor():
 				smp.travel_to('Floored')
-		"Crouch":
-			var crouch = Input.is_action_pressed('ui_down')
-			velocity.x = 0
-			if not crouch:
-				smp.travel_to('Floored')
 		"Dash":
 			animated_sprite.play('Dash')
 			velocity.y = 0
 			velocity.x = dash_direction * dash_speed
 			dash_timer += delta
-			if dash_timer > dash_anim_duration:
+			if dash_timer > dash_duration:
+				dash_timer = 0
 				flipped_dash_trail.emitting = false
 				dash_trail.emitting = false
 				smp.travel_to("Airborne")
 				
 	
-func physics_process_state(state: String, delta: float):
-	match state:
+func _physics_process(delta: float):
+	match smp.state:
 		"Floored", "Airborne":
 			if not is_zero_approx(velocity.y):
 				smp.travel_to('Airborne')
 			process_horizontal_move()
-			process_jump_input()
+			process_jump_input(delta)
 			continue
 		"Floored":
-			var crouch = Input.is_action_pressed('ui_down')
 			animated_sprite.play('Idle' if is_zero_approx(velocity.x) else 'Run')
-			if crouch:
-				smp.travel_to('Crouch')
 
-	if state != 'Dead':
+	if smp.state != 'Dead':
 		velocity.y += gravity * delta
 		velocity = move_and_slide(velocity, Vector2(0, -1))
 		process_invincibility(delta)
---
+++ utils/state_machine.gd
@@ -0,0 +1,36 @@
+class_name StateMachine
+extends Node
+
+var state: String
+var entity: Node
+var time_since_states := {}
+
+
+func _init(parent, initial_state, states):
+	entity = parent
+	entity.add_child(self)
+	state = initial_state
+	for possible_state in states:
+		time_since_states[possible_state] = 0.0
+
+
+func _ready() -> void:
+	if entity.has_method("transited_state"):
+		entity.transited_state(null, state)
+
+
+func _physics_process(delta: float) -> void:
+	for possible_state in time_since_states.keys():
+		if state == possible_state:
+			time_since_states[possible_state] = 0.0
+		time_since_states[possible_state] += delta
+
+
+func travel_to(new_state: String) -> void:
+	if state != new_state:
+		state = new_state
+		entity.transited_state(state, new_state)
+
+
+func time_since(_state: String) -> float:
+	return time_since_states[_state]
