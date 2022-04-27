6c4d61347bd37a0fe32abdd07e275c5c301f1e01
Julián Absatz <julianabsatz@gmail.com>
2022-04-27 02:22:54 +0200

    intro

---
+++ demos/glassbrick_intro.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+export (String) var next_scene_path = null
+
+
+func _ready():
+	$AnimationPlayer.play('arrow clank clank')
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed('ui_accept') and not SceneManager.is_transitioning:
+		SceneManager.change_scene(next_scene_path)
+
+
+func _on_AnimationPlayer_animation_finished(_animation_name):
+	SceneManager.change_scene(next_scene_path)
