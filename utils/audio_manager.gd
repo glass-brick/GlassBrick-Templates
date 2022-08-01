ce712a86a7eb8f79c93f7b66dd0feeb03ad69d67
Julián Absatz <julianabsatz@gmail.com>
2022-08-01 22:44:03 +0200

    usar funcion nativa para el audio

--- utils/audio_manager.gd
+++ utils/audio_manager.gd
@@ -1,9 +1,5 @@
 extends Node
 
-const min_volume = -80.0
-# when doing `log(0.00001) * logarithm_magic_number`, the result should approach -80.0 as much as possible
-const logarithm_magic_number = 20.0 / log(10.0)
-
 # All this should be in an autoload since bgm/se should be accessible always
 # Use setup_slider(slider, "bgm"/"se") to make a slider functional for volume change in settings
 # Add the bus name to every bgm/se you use to make them react to the global volume
@@ -37,16 +33,10 @@ func get_volume(type):
 	return audio_data[type]["volume"]
 
 
-func volume_to_logarithmic(amount: float):
-	if is_zero_approx(amount):
-		return min_volume
-	return log(amount) * logarithm_magic_number
-
-
 func set_volume(local_value: float, audio_type: String, should_save: bool = true):
 	audio_data[audio_type]["volume"] = local_value
 
-	var real_volume = volume_to_logarithmic(local_value)
+	var real_volume = linear2db(local_value)
 	var audio_index = AudioServer.get_bus_index(audio_data[audio_type]["bus_name"])
 
 	if audio_index != -1:
