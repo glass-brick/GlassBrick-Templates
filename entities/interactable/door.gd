1a47749389d3c817c6b9bc0a90e611d02f4513a8
Julián Absatz <julianabsatz@gmail.com>
2022-08-02 14:53:33 +0200

    customizable door

--- entities/interactable/door.gd
+++ entities/interactable/door.gd
@@ -1,7 +1,8 @@
-extends TileMap
+extends Node2D
+tool
 
 var open := false
-export (int) var height := 64
+export (int) var height := 4 setget set_height
 export (float) var time := 1.0
 var tween := Tween.new()
 onready var interactable: Interactable = $Interactable
@@ -19,9 +20,12 @@ const TransitionTypes := {
 	'BACK': Tween.TRANS_BACK,
 }
 export (TransitionTypes) var transition_type = TransitionTypes.CUBIC
+onready var tilemap: TileMap = $Tilemap
 
 
 func _ready():
+	if Engine.editor_hint:
+		return
 	interactable.connect("interacted", self, "interact")
 	add_child(tween)
 
@@ -37,7 +41,12 @@ func interact():
 func open_door():
 	interactable.disable()
 	tween.interpolate_property(
-		self, "position", self.position, self.position + Vector2.UP * height, time, transition_type
+		self,
+		"position",
+		self.position,
+		self.position + Vector2.UP * height * tilemap.cell_size,
+		time,
+		transition_type
 	)
 	tween.start()
 	yield(tween, "tween_completed")
@@ -51,7 +60,7 @@ func close_door():
 		self,
 		"position",
 		self.position,
-		self.position + Vector2.DOWN * height,
+		self.position + Vector2.DOWN * height * tilemap.cell_size,
 		time,
 		transition_type
 	)
@@ -59,3 +68,13 @@ func close_door():
 	yield(tween, "tween_completed")
 	open = false
 	interactable.enable()
+
+
+func set_height(new_height):
+	height = new_height
+	$Tilemap.clear()
+	for y in range(height):
+		$Tilemap.set_cell(0, y, 0)
+	$Tilemap.update_bitmask_region(Vector2.ZERO, Vector2(1, height))
+	$Interactable.collision_shape.extents = $Tilemap.cell_size * Vector2(0.5, height * 0.5)
+	$Interactable.position = $Interactable.collision_shape.extents
